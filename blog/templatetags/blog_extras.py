2bd3d6b65bef8118f881fce1769580e4bcb8e2a8
Ivan Egorov <iegorov@sfedu.ru>
2023-02-09 09:14:14 +0000

    Finish crispy forms

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -31,11 +31,19 @@ def author_details(author, current_user=None):
         return format_html('{}{}{}',prefix,name,suffix)
 
 @register.simple_tag
-def row():
+def row(extra_classes=""):
     return format_html('<div class="row">')
 
 @register.simple_tag
 def endrow():
+    return format_html('</div>')
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col">')
+
+@register.simple_tag
+def endcol():
     return format_html('</div>')
 
 @register.inclusion_tag("blog/post-list.html")
