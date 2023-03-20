61d2e1415db33d30c287ad153f4977b809e3d5a7
Ivan Egorov <iegorov@sfedu.ru>
2023-03-20 09:23:40 +0000

    Finish testing DRF with mocks

--- blango_auth/models.py
+++ blango_auth/models.py
@@ -13,9 +13,9 @@ class BlangoUserManager(UserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, passwod=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
-        extra_fields.setdafault("is_superuser", False)
+        extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
